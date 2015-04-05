d6dbc878a1bde0ad7bc0f291e403a469a19e30a9
Layla <fortblox@gmail.com>
2015-04-05 23:01:50 +0100

    Transforming wheel objects to the wheel colliders

--- Assets/Scripts/Behaviours/Vehicles/Vehicle_Physics.cs
+++ Assets/Scripts/Behaviours/Vehicles/Vehicle_Physics.cs
@@ -14,15 +14,23 @@ namespace SanAndreasUnity.Behaviours.Vehicles
 
             rb.mass = 1000f;
 
-            foreach (var wheel in _wheels) {
-                var col = wheel.AddComponent<WheelCollider>();
+            foreach (var wheel in _wheels)
+            {
+                wheel.Collider = wheel.Parent.gameObject.AddComponent<WheelCollider>();
+                wheel.Collider.radius = 0.35f;
+                wheel.Collider.suspensionDistance = 0.2f;
+
+                var spring = wheel.Collider.suspensionSpring;
+                spring.targetPosition = 1.0f;
+                wheel.Collider.suspensionSpring = spring;
             }
         }
 
         private void FixedUpdate()
         {
-            foreach (var wheel in _wheels) {
-                wheel.GetComponent<WheelCollider>().motorTorque = MotorTorque;
+            foreach (var wheel in _wheels)
+            {
+                wheel.Collider.motorTorque = MotorTorque;
             }
         }
     }
