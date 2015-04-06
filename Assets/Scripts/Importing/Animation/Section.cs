36103a05d5c109ca2730ce2f092193bb8a0657d4
James King <james.king@facepunchstudios.com>
2015-04-06 18:29:36 +0100

    Started writing an animation package parser.

---
+++ Assets/Scripts/Importing/Animation/Section.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+using SanAndreasUnity.Utilities;
+
+namespace SanAndreasUnity.Importing.Animation
+{
+    public abstract class Section
+    {
+        public readonly string Identifier;
+        public readonly long Length;
+
+        protected Section(BinaryReader reader)
+        {
+            Identifier = reader.ReadString(4);
+            Length = reader.ReadUInt32();
+        }
+    }
+
+    public class AnimationPackage : Section
+    {
+
+    }
+}
