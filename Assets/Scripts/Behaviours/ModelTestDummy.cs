4469389b428bdc3a2fb6ce595fc309caba7fb9aa
James King <james.king@facepunchstudios.com>
2015-04-06 18:08:40 +0100

    Can now change the test model name while paused and it will reload immediately.

--- Assets/Scripts/Behaviours/ModelTestDummy.cs
+++ Assets/Scripts/Behaviours/ModelTestDummy.cs
@@ -4,11 +4,13 @@ using System.Linq;
 using System.Text;
 using SanAndreasUnity.Behaviours.World;
 using SanAndreasUnity.Importing.Conversion;
+using UnityEditor;
 using UnityEngine;
 
 namespace SanAndreasUnity.Behaviours
 {
     [RequireComponent(typeof(MeshFilter), typeof(MeshRenderer))]
+    [ExecuteInEditMode]
     public class ModelTestDummy : MonoBehaviour
     {
         private string _loadedModelName;
@@ -19,12 +21,21 @@ namespace SanAndreasUnity.Behaviours
         private void Update()
         {
             if (Cell.GameData == null) return;
+#if UNITY_EDITOR
+            if (!EditorApplication.isPlaying && !EditorApplication.isPaused) return;
+#endif
 
             if (_loadedModelName != ModelName) {
                 _loadedModelName = ModelName;
 
-                LoadModel(ModelName, TextureDictionaries.ToArray());
+                LoadModel(ModelName, TextureDictionaries.Count == 0
+                    ? new[] { ModelName } : TextureDictionaries.ToArray());
             }
+        }
+        
+        private void OnValidate()
+        {
+            Update();
         }
 
         private void LoadModel(string modelName, params string[] txds)
