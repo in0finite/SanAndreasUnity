d31fe00c139e27e83f9c16b1193cd465c525145f
Layla <fortblox@gmail.com>
2015-04-06 23:01:13 +0100

    Ped test using skinned mesh renderer. Started reading skin section data.

---
+++ Assets/Scripts/Importing/RenderWareStream/Skin.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+
+namespace SanAndreasUnity.Importing.RenderWareStream
+{
+    [SectionType(0x0116)]
+    public class Skin : SectionData
+    {
+        public Skin(SectionHeader header, Stream stream)
+        {
+            var reader = new BinaryReader(stream);
+
+            Int32 boneCount = (Int32)reader.ReadByte();
+            Int32 boneIdCount = (Int32)reader.ReadByte();
+            UInt16 weightsPerVertex = reader.ReadUInt16();
+
+            byte[] boneIds = reader.ReadBytes(boneIdCount);
+        }
+    }
+}
