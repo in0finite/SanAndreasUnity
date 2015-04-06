756c8df88a39e8a1c2a27a0e0a4082d53591beab
James King <james.king@facepunchstudios.com>
2015-04-06 23:34:13 +0100

    Added `Utilities.Profiler`, refactored loader timing.

--- Assets/Scripts/Behaviours/Loader.cs
+++ Assets/Scripts/Behaviours/Loader.cs
@@ -27,62 +27,45 @@ namespace SanAndreasUnity.Behaviours
         {
             if (HasLoaded) return;
 
-            var timer = new Stopwatch();
-
             var archivePaths = Config.Get("archive_paths")
                 .Select(x => FormatPath((string) x))
                 .ToArray();
 
-            timer.Start();
-            var archives = archivePaths.Select(x => 
-                File.Exists(x) ? (IArchive) ArchiveManager.LoadImageArchive(x)
-                : Directory.Exists(x) ? ArchiveManager.LoadLooseArchive(x)
-                : null).Where(x => x != null).ToArray();
-            timer.Stop();
-
-            UnityEngine.Debug.LogFormat("Archive load time: {0} ms", timer.Elapsed.TotalMilliseconds);
-            timer.Reset();
-
-            timer.Start();
-            foreach (var archive in archives) {
-                foreach (var colFile in archive.GetFileNamesWithExtension(".col")) {
-                    CollisionFile.Load(colFile);
-                }
+            IArchive[] archives;
+            using (Utilities.Profiler.Start("Archive load time")) {
+                archives = archivePaths.Select(x => 
+                    File.Exists(x) ? (IArchive) ArchiveManager.LoadImageArchive(x)
+                    : Directory.Exists(x) ? ArchiveManager.LoadLooseArchive(x)
+                    : null).Where(x => x != null).ToArray();
             }
-            timer.Stop();
-
-            UnityEngine.Debug.LogFormat("Collision load time: {0} ms", timer.Elapsed.TotalMilliseconds);
-            timer.Reset();
-
-            timer.Start();
-
-            foreach (var path in Config.Get("item_paths").Select(x => FormatPath((string) x))) {
-                var ext = Path.GetExtension(path).ToLower();
-                switch (ext) {
-                    case ".dat":
-                        Item.ReadLoadList(path); break;
-                    case ".ide":
-                        Item.ReadIde(path); break;
-                    case ".ipl":
-                        Item.ReadIpl(path); break;
+            
+            using (Utilities.Profiler.Start("Collision load time")) {
+                foreach (var archive in archives) {
+                    foreach (var colFile in archive.GetFileNamesWithExtension(".col")) {
+                        CollisionFile.Load(colFile);
+                    }
                 }
             }
 
-            timer.Stop();
-
-            UnityEngine.Debug.LogFormat("Item info load time: {0} ms", timer.Elapsed.TotalMilliseconds);
-            timer.Reset();
-
-            timer.Start();
-
-            foreach (var path in Config.Get("handling_paths").Select(x => FormatPath((string) x))) {
-                Handling.Load(path);
+            using (Utilities.Profiler.Start("Item info load time")) {
+                foreach (var path in Config.Get("item_paths").Select(x => FormatPath((string) x))) {
+                    var ext = Path.GetExtension(path).ToLower();
+                    switch (ext) {
+                        case ".dat":
+                            Item.ReadLoadList(path); break;
+                        case ".ide":
+                            Item.ReadIde(path); break;
+                        case ".ipl":
+                            Item.ReadIpl(path); break;
+                    }
+                }
             }
 
-            timer.Stop();
-
-            UnityEngine.Debug.LogFormat("Handling load time: {0} ms", timer.Elapsed.TotalMilliseconds);
-            timer.Reset();
+            using (Utilities.Profiler.Start("Handling info load time")) {
+                foreach (var path in Config.Get("handling_paths").Select(x => FormatPath((string) x))) {
+                    Handling.Load(path);
+                }
+            }
 
             HasLoaded = true;
         }
