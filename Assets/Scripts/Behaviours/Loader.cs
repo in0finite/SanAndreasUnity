83d2ae416226e24e203519a1575b25871efde825
Thomas Buck <xythobuz@xythobuz.de>
2017-03-30 23:36:04 +0200

    Added very crude and simple Minimap radar. Only displays the current north-aligned map tile with properly rotated player blip.

--- Assets/Scripts/Behaviours/Loader.cs
+++ Assets/Scripts/Behaviours/Loader.cs
@@ -6,126 +6,44 @@ using SanAndreasUnity.Importing.Collision;
 using SanAndreasUnity.Importing.Items;
 using SanAndreasUnity.Importing.Vehicles;
 using SanAndreasUnity.Utilities;
+using SanAndreasUnity.Behaviours;
 using UnityEngine;
 #if UNITY_EDITOR
 using UnityEditor;
 #endif
 using System.Collections.Generic;
 
-namespace SanAndreasUnity.Behaviours
-{
-	#if UNITY_EDITOR
+namespace SanAndreasUnity.Behaviours {
+#if UNITY_EDITOR
 //	[ExecuteInEditMode]
 	[InitializeOnLoad]
-	#endif
-    public class Loader : MonoBehaviour
-    {
-	//	public	bool	asynchronousLoad = false;
-
+#endif
+    public class Loader : MonoBehaviour {
         public static bool HasLoaded { get; private set; }
-		public	static	string	loadingStatusString {
+		public static string loadingStatusString {
 			get { 
 				string[] statuses = { "archive paths", "archive", "collision files", "item info",
-					"handling info", "animation group info", "car colors" };
+					"handling info", "animation group info", "car colors", "special textures" };
 			
 				return "Loading " + statuses [loadingStatus];
 			}
 		}
-		private	static	int	loadingStatus = 0 ;
-		private	static	string[]	archivePaths;
-		private	static	IArchive[] archives;
+		private static int loadingStatus = 0 ;
+		private static string[] archivePaths;
+		private static IArchive[] archives;
 
-
-		static	Loader() {
-			// called on editor startup
-			// start loading game data
-
-		//	EditorApplication.update += StaticUpdate;
-
-		}
-
-        private void Awake()
-        {
-            if (HasLoaded) return;
-			
-			/*
-			var archivePaths = Config.GetPaths ("archive_paths");
-
-			IArchive[] archives;
-			using (Utilities.Profiler.Start("Archive load time")) {
-				archives = archivePaths.Select(x => 
-					File.Exists(x) ? (IArchive) ArchiveManager.LoadImageArchive(x)
-					: Directory.Exists(x) ? ArchiveManager.LoadLooseArchive(x)
-					: null).Where(x => x != null).ToArray();
-			}
-
-			using (Utilities.Profiler.Start("Collision load time")) {
-				foreach (var archive in archives) {
-					foreach (var colFile in archive.GetFileNamesWithExtension(".col")) {
-						CollisionFile.Load(colFile);
-					}
-				}
-			}
-
-			using (Utilities.Profiler.Start("Item info load time")) {
-				foreach (var path in Config.GetPaths("item_paths")) {
-					var ext = Path.GetExtension(path).ToLower();
-					switch (ext) {
-					case ".dat":
-						Item.ReadLoadList(path); break;
-					case ".ide":
-						Item.ReadIde(path); break;
-					case ".ipl":
-						Item.ReadIpl(path); break;
-					}
-				}
-			}
-
-			using (Utilities.Profiler.Start("Handling info load time")) {
-				Handling.Load(Config.GetPath("handling_path"));
-			}
-
-			using (Utilities.Profiler.Start("Animation group info load time")) {
-				foreach (var path in Config.GetPaths("anim_groups_paths")) {
-					AnimationGroup.Load(path);
-				}
-			}
-
-			using (Utilities.Profiler.Start("Car color info load time")) {
-				CarColors.Load(Config.GetPath("car_colors_path"));
-			}
-
-			HasLoaded = true;
-			*/
-
-        }
-
-		static	void StaticUpdate() {
-
-			#if UNITY_EDITOR
-			if (!EditorApplication.isPlaying && !HasLoaded) {
-				// display loading progress in editor
-
-			}
-			#endif
-
+		static void StaticUpdate() {
 			if (HasLoaded)
 				return;
 
-
 			switch (loadingStatus) {
-
 			case 0:
-
 				Debug.Log ("Started loading GTA.");
-
 				archivePaths = Config.GetPaths ("archive_paths");
-
 				break;
+
 			case 1:
-				
 				using (Utilities.Profiler.Start ("Archive load time")) {
-					
 					List<IArchive> listArchives = new List<IArchive> ();
 					foreach( var path in archivePaths) {
 						if(File.Exists(path)) {
@@ -137,12 +55,10 @@ namespace SanAndreasUnity.Behaviours
 						}
 					}
 					archives = listArchives.FindAll (a => a != null).ToArray ();
-
 				}
-
 				break;
-			case 2:
 
+			case 2:
 				using (Utilities.Profiler.Start ("Collision load time")) {
 					int numCollisionFiles = 0;
 					foreach (var archive in archives) {
@@ -153,10 +69,9 @@ namespace SanAndreasUnity.Behaviours
 					}
 					Debug.Log ("Number of collision files " + numCollisionFiles);
 				}
-
 				break;
-			case 3:
 
+			case 3:
 				using (Utilities.Profiler.Start ("Item info load time")) {
 					foreach (var path in Config.GetPaths("item_paths")) {
 						var ext = Path.GetExtension (path).ToLower ();
@@ -173,65 +88,55 @@ namespace SanAndreasUnity.Behaviours
 						}
 					}
 				}
-
 				break;
-			case 4:
 
+			case 4:
 				using (Utilities.Profiler.Start ("Handling info load time")) {
 					Handling.Load (Config.GetPath ("handling_path"));
 				}
-
 				break;
-			case 5:
 
+			case 5:
 				using (Utilities.Profiler.Start ("Animation group info load time")) {
 					foreach (var path in Config.GetPaths("anim_groups_paths")) {
 						AnimationGroup.Load (path);
 					}
 				}
-
 				break;
-			case 6:
 
+			case 6:
 				using (Utilities.Profiler.Start ("Car color info load time")) {
 					CarColors.Load (Config.GetPath ("car_colors_path"));
 				}
+				break;
 
+			case 7:
+				using (Utilities.Profiler.Start ("special texture load time")) {
+					MiniMap.loadTextures ();
+				}
 				HasLoaded = true;
-
 				Debug.Log ("GTA loading finished.");
-
 				break;
 
 			}
 
 			loadingStatus++;
-
-
 		}
 
 		void Update() {
-
 			StaticUpdate ();
-
 		}
 
-		void	OnGUI() {
-
+		void OnGUI() {
 			if (HasLoaded)
 				return;
 
 			// display loading progress
-
 			GUILayout.BeginHorizontal ();
 			GUILayout.FlexibleSpace ();
 			GUILayout.Label ("<size=25>" + loadingStatusString + "</size>");
 			GUILayout.FlexibleSpace ();
 			GUILayout.EndHorizontal ();
-
 		}
-
-
 	}
-
 }
---
+++ Assets/Scripts/Behaviours/MiniMap.cs
@@ -0,0 +1,115 @@
+﻿using UnityEngine;
+using SanAndreasUnity.Importing.Conversion;
+
+namespace SanAndreasUnity.Behaviours {
+	[RequireComponent(typeof(Player))]
+	public class MiniMap : MonoBehaviour {
+		const int tileEdge = 12; // width/height of map in tiles
+		const int tileCount = tileEdge * tileEdge; // number of tiles
+		const int mapEdge = 6000; // width/height of map in world coordinates
+		const int texSize = 128; // width/height of single tile in px
+		const int mapSize = tileEdge * texSize; // width/height of whole map in px
+
+		static TextureDictionary[] tiles = new TextureDictionary[tileCount];
+		static TextureDictionary huds;
+		static Texture2D northBlip;
+		static Texture2D playerBlip;
+
+		public static void loadTextures() {
+			for (int i = 0; i < tileCount; i++) {
+				tiles [i] = TextureDictionary.Load ("radar" + ((i < 10) ? "0" : "") + i);
+			}
+
+			huds = TextureDictionary.Load ("hud");
+			northBlip = huds.GetDiffuse ("radar_north").Texture;
+			playerBlip = huds.GetDiffuse ("radar_centre").Texture;
+		}
+
+		private static Texture2D getTile(int i) {
+			if ((i < 0) || (i >= tileCount))
+				return null;
+
+			if (!Loader.HasLoaded)
+				return null;
+
+			return tiles [i].GetDiffuse ("radar" + ((i < 10) ? "0" : "") + i).Texture;
+		}
+
+		private static void drawTexturePart(Texture2D tex, Vector2 pos, Vector2 tileStart, Vector2 tileSize) {
+			GUI.DrawTextureWithTexCoords (new Rect (pos, tileSize), tex,
+				new Rect (tileStart.x / (float)tex.width, (tileSize.y - tileStart.y) / (float)tex.height, tileSize.x / (float)tex.width, -tileSize.y / (float)tex.height));
+		}
+
+		// Draw a tile at screen position pos with size tileSize, starting with tileSize pixels in the tile at tileStart
+		private static void drawTilePart(int tile, Vector2 pos, Vector2 tileStart, Vector2 tileSize) {
+			Texture2D tex = getTile (tile);
+			if (tex != null) {
+				drawTexturePart (tex, pos, tileStart, tileSize);
+			}
+		}
+
+		private static Vector2 coordinatesWorldToPixel(Vector2 pos) {
+			// Convert from world-space (0 - 6000) to map pixel space (0 - 1536)
+			return new Vector2 (pos.x / (float)mapEdge * (float)mapSize, pos.y / (float)mapEdge * (float)mapSize);
+		}
+
+		private static int coordinatesToTileNumber(Vector2 pos) {
+			int x = (int)pos.x * tileEdge / mapSize;
+			int y = (int)pos.y * tileEdge / mapSize;
+			int tile = x + (tileEdge * y);
+			return tile < tileCount ? tile : tileCount - 1;
+		}
+
+		private static Vector2 coordinatesInTile(Vector2 pos) {
+			if (pos.x >= mapSize) {
+				pos.x = mapSize - 1;
+			}
+			if (pos.y >= mapSize) {
+				pos.y = mapSize - 1;
+			}
+			return new Vector2 (pos.x % texSize, pos.y % texSize);
+		}
+
+		// --------------------------------
+
+		#region Private fields
+		private Player player;
+		private PlayerController playerController;
+		#endregion
+
+		void Awake () {
+			player = GetComponent<Player>();
+			playerController = GetComponent<PlayerController>();
+		}
+
+		private void drawMapWindow(Vector2 screenPos, Vector2 mapPos) {
+			// Draw current map tile
+			Vector2 pxPos = coordinatesWorldToPixel (mapPos);
+			drawTilePart (coordinatesToTileNumber (pxPos), screenPos, new Vector2 (0, 0), new Vector2 (texSize, texSize));
+
+			// Draw player blip
+			Vector2 tilePos = coordinatesInTile (pxPos);
+			Matrix4x4 matrixBackup = GUI.matrix;
+			GUIUtility.RotateAroundPivot(player.transform.rotation.eulerAngles.y, new Vector2 (screenPos.x + tilePos.x, screenPos.y + tilePos.y));
+			drawTexturePart (playerBlip, new Vector2 (screenPos.x + tilePos.x - (playerBlip.width / 2), screenPos.y + tilePos.y - (playerBlip.height / 8)), new Vector2 (0, 0), new Vector2 (playerBlip.width, playerBlip.height));
+			GUI.matrix = matrixBackup;
+
+			// Draw 'N' north marker at top of map
+			drawTexturePart (northBlip, new Vector2 (screenPos.x + ((texSize - northBlip.width) / 2), screenPos.y - (northBlip.height / 2)), new Vector2 (0, 0), new Vector2 (northBlip.width, northBlip.height));
+		}
+
+		void OnGUI() {
+			if (!Loader.HasLoaded) return;
+			if (!playerController.CursorLocked) return;
+
+			// Player coordinates on map, (0; 0) moved from center to top-left
+			Vector2 pos = new Vector2 (player.transform.position.x + (mapEdge / 2), mapEdge - (player.transform.position.z + (mapEdge / 2)));
+
+			GUILayout.BeginArea (new Rect (10, 10, 150, 25));
+			GUILayout.Label ("Pos: " + (int)pos.x + " " + (int)pos.y + " (" + (int)player.transform.position.y + ")");
+			GUILayout.EndArea ();
+
+			drawMapWindow (new Vector2 (10, 40), pos);
+		}
+	}
+}
