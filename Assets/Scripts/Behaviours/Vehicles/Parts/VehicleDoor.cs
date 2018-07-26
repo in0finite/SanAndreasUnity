c9c3c336457a4d75832c925a5a9655e00b89e49c
Ikillnukes <ikillnukes1@gmail.com>
2018-07-26 20:54:06 +0200

    Working on doors probabilities

--- Assets/Scripts/Behaviours/Vehicles/Parts/VehicleDoor.cs
+++ Assets/Scripts/Behaviours/Vehicles/Parts/VehicleDoor.cs
@@ -5,13 +5,32 @@ using UnityEngine;
 
 public class VehicleDoor : MonoBehaviour
 {
+    private bool _isLocked;
+    public bool isLocked
+    {
+        get
+        {
+            return _isLocked;
+        }
 
-    public float drag;
+        set
+        {
+            _isLocked = value;
+            body.constraints = _isLocked ? RigidbodyConstraints.FreezeRotationY : RigidbodyConstraints.None;
+        }
+    }
+
+    public float force;
 
     private float lastForce;
     private bool allowedToDebug;
     private Rigidbody body, vehicleBody;
     private Vehicle vehicle;
+
+    private float lockHealth = 100;
+    private HingeJoint joint;
+    //private MeshCollider okCollider, damCollider;
+    private NonConvexMeshCollider okCollider, damCollider;
 
     public static VehicleDoor InitializateDoor(Transform door, Vehicle vehicle)
     {
@@ -29,19 +48,34 @@ public class VehicleDoor : MonoBehaviour
 	void Start ()
     {
         body = GetComponent<Rigidbody>();
-	}
+        joint = GetComponent<HingeJoint>();
+
+        _isLocked = true;
+
+        joint.breakForce = 5000 / vehicle.HandlingData.CollisionDamageMult;
+
+        string prefix = transform.name.Substring(0, 7);
+
+        okCollider = transform.Find(string.Format("{0}_ok", prefix)).gameObject.AddComponent<NonConvexMeshCollider>();
+        damCollider = transform.Find(string.Format("{0}_dam", prefix)).gameObject.AddComponent<NonConvexMeshCollider>();
+    }
 	
 	// Update is called once per frame
 	void Update ()
     {
         //drag = body.angularVelocity.magnitude;
 
-        drag = body.mass * Mathf.Pow(vehicleBody.angularVelocity.magnitude, 2) * Vector3.Distance(vehicle.transform.TransformPoint(vehicleBody.centerOfMass), transform.position);
+        force = (vehicleBody.mass * .015f) * Mathf.Pow(vehicleBody.angularVelocity.magnitude, 2) * Vector3.Distance(vehicle.transform.TransformPoint(vehicleBody.centerOfMass), transform.position);
 
-        if (drag > lastForce && allowedToDebug)
+        if (force > 100 && Random.value < lockHealth / 100f)
+            _isLocked = false;
+
+        // If rotation from the hinge is 0 (== can be closed) block the door
+
+        if (force > lastForce && allowedToDebug)
         {
-            Debug.Log("NEW MAX: " + drag);
-            lastForce = drag;
+            //Debug.Log("NEW MAX: " + drag);
+            lastForce = force;
         }
     }
 }
