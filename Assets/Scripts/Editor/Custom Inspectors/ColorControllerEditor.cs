b07d0ec6fb8206f26989e2778e49a9af34803bf6
Ikillnukes <ikillnukes1@gmail.com>
2018-08-10 00:59:41 +0200

    Remove unnecesary lines

--- Assets/Scripts/Editor/Custom Inspectors/ColorControllerEditor.cs
+++ Assets/Scripts/Editor/Custom Inspectors/ColorControllerEditor.cs
@@ -6,38 +6,6 @@ public class ColorControllerEditor
 {
     public static void OnInspectorGUI(ColorController target, SerializedObject so)
     {
-        so.UpdateIfRequiredOrScript();
-
-        /*var dictionary = so.FindProperty("_mapColor");
-
-        if (target.serializedMapColor != null && target.serializedMapColor != null)
-        {
-            //if (GUILayout.Button("New value"))
-            //    target.serializedMapColor.Add(Color.white, 0);
-
-            EditorGUILayout.PropertyField(dictionary);
-
-            /*for (int i = 0; i < target.serializedMapColor.Count; ++i)
-            {
-                var element = dictionary.GetFixedBufferElementAtIndex(i);
-
-                GUILayout.BeginHorizontal();
-
-                Color key = (Color)(element.FindPropertyRelative("key").objectReferenceValue as object);
-                element.FindPropertyRelative("key").objectReferenceValue = EditorGUILayout.ColorField(key) as object as Object;
-
-                float value = (float)(element.FindPropertyRelative("value").objectReferenceValue as object);
-                element.FindPropertyRelative("value").objectReferenceValue = EditorGUILayout.FloatField(value) as object as Object;
-
-                if (GUILayout.Button("Remove value"))
-                    target.serializedMapColor.Remove(target.serializedMapColor.Keys.ElementAt(i));
-
-                GUILayout.EndHorizontal();
-            }
-        }
-
-        so.ApplyModifiedProperties();*/
-
         if (GUILayout.Button("Re-init all values"))
         {
             target._mapColor.Clear();
@@ -45,9 +13,6 @@ public class ColorControllerEditor
             foreach (var kv in ZHelpers.mapColors)
                 target._mapColor.Add(kv.Value, 0);
         }
-
-        //myTarget.serializedMapColor = EditorGUILayout.IntField("Experience", myTarget.experience);
-        //EditorGUILayout.LabelField("Level", myTarget.Level.ToString());
     }
 }
 
@@ -57,7 +22,7 @@ public class StarControllerEditor : Editor
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-        ColorControllerEditor.OnInspectorGUI((ColorController)target, base.serializedObject);
+        ColorControllerEditor.OnInspectorGUI((ColorController)target, serializedObject);
     }
 }
 
@@ -67,6 +32,6 @@ public class WeatherControllerEditor : Editor
     public override void OnInspectorGUI()
     {
         DrawDefaultInspector();
-        ColorControllerEditor.OnInspectorGUI((ColorController)target, base.serializedObject);
+        ColorControllerEditor.OnInspectorGUI((ColorController)target, serializedObject);
     }
 }
