ec736a1d67d675c1ea92d1c9b685f4b4b9b70976
Ikillnukes <ikillnukes1@gmail.com>
2018-08-09 17:18:37 +0200

    Min Max Impl

---
+++ Assets/Scripts/Utilities/GUI/MinMaxAttribute.cs
@@ -0,0 +1,17 @@
+﻿// NOTE DONT put in an editor folder
+
+using UnityEngine;
+
+public class MinMaxAttribute : PropertyAttribute
+{
+    public float MinLimit = 0;
+    public float MaxLimit = 1;
+    public bool ShowEditRange;
+    public bool ShowDebugValues;
+
+    public MinMaxAttribute(int min, int max)
+    {
+        MinLimit = min;
+        MaxLimit = max;
+    }
+}
