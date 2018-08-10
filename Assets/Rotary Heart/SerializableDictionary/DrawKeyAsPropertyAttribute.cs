948c02f4281192c03be5d2e2e2bec043b5a15a0e
Ikillnukes <ikillnukes1@gmail.com>
2018-08-10 03:24:40 +0200

    Removed some files

--- Assets/Rotary Heart/SerializableDictionary/DrawKeyAsPropertyAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace RotaryHeart.Lib.SerializableDictionary
-{
-    /// <summary>
-    /// Attribute used to force drawing a key as a property
-    /// </summary>
-    [System.AttributeUsage(System.AttributeTargets.Field)]
-    public class DrawKeyAsPropertyAttribute : System.Attribute { }
-}
