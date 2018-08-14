94fe98b700135e5608cfd3c0f50bafbb45557fbe
in0finite <leshow816@gmail.com>
2018-08-14 16:33:04 +0200

    Time scale can be set in options

---
+++ Assets/Scripts/Settings/MiscSettings.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using SanAndreasUnity.UI;
+using SanAndreasUnity.Utilities;
+
+namespace SanAndreasUnity.Settings {
+
+	public class MiscSettings : MonoBehaviour {
+		
+		OptionsWindow.FloatInput m_timeScaleInput = new OptionsWindow.FloatInput( "Time scale", 0f, 4f );
+
+
+		void Start () {
+
+			UI.OptionsWindow.onGUI += this.OnOptionsGUI;
+
+		}
+
+		void OnOptionsGUI() {
+
+			GUILayout.Label ("\nMISC\n");
+
+
+			m_timeScaleInput.value = Time.timeScale;
+			if (OptionsWindow.FloatSlider (m_timeScaleInput)) {
+				Time.timeScale = m_timeScaleInput.value;
+			}
+
+		}
+
+	}
+
+}
