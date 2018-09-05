117e086d343d7fa0559c1d3151e44463d71d1f4d
in0finite <leshow816@gmail.com>
2018-09-05 22:31:19 +0200

    Created custom inspector for Frame script

---
+++ Assets/Scripts/Editor/FrameInspector.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+namespace SanAndreasUnity.Editor
+{
+
+	[CustomEditor(typeof(Behaviours.Frame))]
+//	[CanEditMultipleObjects]
+	public class FrameInspector : UnityEditor.Editor
+	{
+		
+
+		void OnEnable()
+		{
+			
+		}
+
+		public override void OnInspectorGUI()
+		{
+
+			base.DrawDefaultInspector ();
+
+			GUILayout.Space (10);
+
+			var frame = this.target as Behaviours.Frame;
+
+			EditorGUILayout.LabelField ("Bone id: " + frame.BoneId);
+			EditorGUILayout.LabelField ("Index: " + frame.Index);
+			EditorGUILayout.ObjectField ("Parent: ", frame.Parent, typeof(Behaviours.Frame), true);
+			EditorGUILayout.LabelField ("Parent index: " + frame.ParentIndex);
+			EditorGUILayout.LabelField ("Path: " + frame.Path);
+
+		}
+
+	}
+
+}
