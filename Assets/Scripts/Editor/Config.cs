66a7723af0df6bd5fe40af3dff3d29a25b292f4a
in0finite <leshow816@gmail.com>
2018-09-26 18:11:06 +0200

    Don't throw NotImplementedException when post-processing for non-standalone targets

--- Assets/Scripts/Editor/Config.cs
+++ Assets/Scripts/Editor/Config.cs
@@ -11,26 +11,21 @@ namespace SanAndreasUnity.Editor
         [PostProcessBuild]
         public static void CopyConfig(BuildTarget target, string pathToBuiltProject)
         {
+			if (!IsStandaloneTarget (target))
+				return;
+
             var destDir = Path.GetDirectoryName(pathToBuiltProject);
+
+			// copy config file
 
             var dest = Path.Combine(destDir, Utilities.Config.FileName);
             File.Copy(Utilities.Config.FilePath, dest, true);
 
+			// copy Data folder
+
             var dataDir = SanAndreasUnity.Utilities.Config.DataPath;
 
-            switch (target)
-            {
-                case BuildTarget.StandaloneWindows:
-                case BuildTarget.StandaloneWindows64:
-                case BuildTarget.StandaloneLinux:
-                case BuildTarget.StandaloneLinux64:
-                case BuildTarget.StandaloneLinuxUniversal:
-                    dest = Path.Combine(destDir, Path.GetFileNameWithoutExtension(pathToBuiltProject) + "_Data");
-                    break;
-
-                default:
-                    throw new NotImplementedException(String.Format("Build target '{0}' is not supported.", target));
-            }
+			dest = Path.Combine(destDir, Path.GetFileNameWithoutExtension(pathToBuiltProject) + "_Data");
 
             dest = Path.Combine(dest, "Data");
 
@@ -41,5 +36,22 @@ namespace SanAndreasUnity.Editor
 
             FileUtil.CopyFileOrDirectory(dataDir, dest);
         }
+
+		private static bool IsStandaloneTarget (BuildTarget target)
+		{
+			switch (target)
+			{
+				case BuildTarget.StandaloneWindows:
+				case BuildTarget.StandaloneWindows64:
+				case BuildTarget.StandaloneLinux:
+				case BuildTarget.StandaloneLinux64:
+				case BuildTarget.StandaloneLinuxUniversal:
+				case BuildTarget.StandaloneOSX:
+					return true;
+			}
+
+			return false;
+		}
+
     }
 }
