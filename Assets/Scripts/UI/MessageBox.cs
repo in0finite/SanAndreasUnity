8080caad4472c46f6766a82152d88e4623a00c82
in0finite <leshow816@gmail.com>
2019-04-18 01:33:32 +0200

    Create MessageBox

---
+++ Assets/Scripts/UI/MessageBox.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using SanAndreasUnity.Utilities;
+
+namespace SanAndreasUnity.UI {
+
+	public class MessageBox : PauseMenuWindow
+    {
+
+		public string Title { get; set; }
+		public string Text { get; set; }
+		public bool UseTextField { get; set; }
+
+
+		MessageBox() {
+
+			// set default parameters
+
+			this.windowName = "";
+			this.useScrollView = true;
+			// adjust rect
+			this.windowRect = GUIUtils.GetCenteredRect( new Vector2(400, 300) );
+
+		}
+
+		void Start () {
+			
+		}
+
+		protected override void OnWindowGUI()
+		{
+
+			this.windowName = this.Title;
+
+			if (this.UseTextField)
+				GUILayout.TextField(this.Text);
+			else
+				GUILayout.Label(this.Text);
+
+		}
+
+	}
+}
