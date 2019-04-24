06a2dc4c5f16ea118f4d2e8732dfaa675db20de8
in0finite <leshow816@gmail.com>
2019-04-24 19:17:40 +0200

    Create Player script

---
+++ Assets/Scripts/Networking/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+namespace SanAndreasUnity.Net
+{
+
+    public class Player : NetworkBehaviour
+    {
+
+        static List<Player> s_allPlayers = new List<Player>();
+        public static Player[] AllPlayers { get { return s_allPlayers.ToArray(); } }
+
+
+        void OnEnable()
+        {
+            s_allPlayers.Add(this);
+        }
+
+        void OnDisable()
+        {
+            s_allPlayers.Remove(this);
+        }
+
+        void Start()
+        {
+            
+        }
+
+    }
+
+}
