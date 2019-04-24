80c0507f0b9dc34f9b8bce672389d0be9fc07734
in0finite <leshow816@gmail.com>
2019-04-24 23:02:39 +0200

    Fixing Ped.Instance & Ped.IsLocalPlayer

--- Assets/Scripts/Networking/Player.cs
+++ Assets/Scripts/Networking/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
+using SanAndreasUnity.Behaviours;
 
 namespace SanAndreasUnity.Net
 {
@@ -11,8 +12,12 @@ namespace SanAndreasUnity.Net
         static List<Player> s_allPlayers = new List<Player>();
         public static Player[] AllPlayers { get { return s_allPlayers.ToArray(); } }
 
-        [SyncVar] GameObject m_ownedGameObject;
-        public GameObject OwnedGameObject { get { return m_ownedGameObject; } internal set { m_ownedGameObject = value; } }
+        /// <summary>Local player.</summary>
+        public static Player Local { get; private set; }
+
+        [SyncVar] Ped m_ownedPed;
+        //public GameObject OwnedGameObject { get { return m_ownedGameObject; } internal set { m_ownedGameObject = value; } }
+        public Ped OwnedPed { get { return m_ownedPed; } internal set { m_ownedPed = value; } }
 
 
         void OnEnable()
@@ -23,6 +28,12 @@ namespace SanAndreasUnity.Net
         void OnDisable()
         {
             s_allPlayers.Remove(this);
+        }
+
+        public override void OnStartLocalPlayer()
+        {
+            base.OnStartLocalPlayer();
+            Local = this;
         }
 
         void Start()
