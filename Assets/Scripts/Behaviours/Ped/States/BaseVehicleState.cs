37dc8c7e78f081d5bbb223fa1f0691baf6f9dccd
in0finite <leshow816@gmail.com>
2019-05-06 02:00:53 +0200

    Disable ped transform syncing while in vehicle

--- Assets/Scripts/Behaviours/Ped/States/BaseVehicleState.cs
+++ Assets/Scripts/Behaviours/Ped/States/BaseVehicleState.cs
@@ -21,6 +21,9 @@ namespace SanAndreasUnity.Behaviours.Peds.States
 				m_ped.characterController.enabled = true;
 				m_ped.transform.SetParent(null, true);
 				m_model.IsInVehicle = false;
+				// restore sync interval for network transform
+				if (m_ped.NetTransform != null)
+					m_ped.NetTransform.syncInterval = m_ped.DefaultTransformSyncInterval;
 			}
 
 			if (this.CurrentVehicleSeat != null && this.CurrentVehicleSeat.OccupyingPed == m_ped)
