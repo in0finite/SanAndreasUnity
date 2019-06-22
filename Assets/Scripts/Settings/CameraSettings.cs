dedfd7d75afba2347d68dc49d4b6d3bd0835e9a5
in0finite <leshow816@gmail.com>
2019-06-22 23:19:28 +0200

    Load camera settings on start

--- Assets/Scripts/Settings/CameraSettings.cs
+++ Assets/Scripts/Settings/CameraSettings.cs
@@ -5,21 +5,22 @@ namespace SanAndreasUnity.Settings {
 
 	public class CameraSettings : MonoBehaviour {
 		
+		static float s_farClipPlane = 1000;
+		static float s_fieldOfView = 60;
+
 		OptionsWindow.FloatInput m_farClipPlaneInput = new OptionsWindow.FloatInput() {
 			description = "Far clip plane",
 			minValue = 100,
 			maxValue = 5000,
-			isAvailable = () => Camera.main != null,
-			getValue = () => Camera.main.farClipPlane,
-			setValue = (value) => { Camera.main.farClipPlane = value; },
+			getValue = () => Camera.main != null ? Camera.main.farClipPlane : s_farClipPlane,
+			setValue = (value) => { s_farClipPlane = value; if (Camera.main != null) Camera.main.farClipPlane = value; },
 		};
 		OptionsWindow.FloatInput m_fieldOfViewInput = new OptionsWindow.FloatInput() {
 			description = "Field of view",
 			minValue = 20,
 			maxValue = 120,
-			isAvailable = () => Camera.main != null,
-			getValue = () => Camera.main.fieldOfView,
-			setValue = (value) => { Camera.main.fieldOfView = value; },
+			getValue = () => Camera.main != null ? Camera.main.fieldOfView : s_fieldOfView,
+			setValue = (value) => { s_fieldOfView = value; if (Camera.main != null) Camera.main.fieldOfView = value; },
 		};
 
 
@@ -27,6 +28,20 @@ namespace SanAndreasUnity.Settings {
 		void Awake ()
 		{
 			OptionsWindow.RegisterInputs ("CAMERA", m_farClipPlaneInput, m_fieldOfViewInput);
+			UnityEngine.SceneManagement.SceneManager.activeSceneChanged += (s1, s2) => OnActiveSceneChanged();
+		}
+
+		void OnActiveSceneChanged()
+		{
+			// apply settings
+
+			Camera cam = Utilities.F.FindMainCameraEvenIfDisabled();
+			if (cam != null)
+			{
+				cam.farClipPlane = s_farClipPlane;
+				cam.fieldOfView = s_fieldOfView;
+			}
+			
 		}
 
 	}
