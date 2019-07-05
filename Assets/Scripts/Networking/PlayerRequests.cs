ab71344f6eb7d0a41fea710065272ff796345021
in0finite <leshow816@gmail.com>
2019-07-06 00:10:09 +0200

    working on player requests 2

--- Assets/Scripts/Networking/PlayerRequests.cs
+++ Assets/Scripts/Networking/PlayerRequests.cs
@@ -3,6 +3,8 @@ using UnityEngine;
 using Mirror;
 using SanAndreasUnity.Utilities;
 using SanAndreasUnity.Behaviours;
+using SanAndreasUnity.Behaviours.Vehicles;
+using System.Linq;
 
 namespace SanAndreasUnity.Net
 {
@@ -80,6 +82,46 @@ namespace SanAndreasUnity.Net
 
             m_timeWhenChangedPedModel = Time.time;
             F.RunExceptionSafe( () => m_player.OwnedPed.PlayerModel.Load(Ped.RandomPedId) );
+        }
+
+        public void RequestSuicide()
+        {
+            this.CmdRequestSuicide();
+        }
+
+        [Command]
+        void CmdRequestSuicide()
+        {
+            if (m_player.OwnedPed != null)
+                Destroy(m_player.OwnedPed.gameObject);
+        }
+
+        public void RequestToDestroyAllVehicles()
+        {
+            this.CmdRequestToDestroyAllVehicles();
+        }
+
+        [Command]
+        void CmdRequestToDestroyAllVehicles()
+        {
+            foreach (var v in Vehicle.AllVehicles.ToArray())
+            {
+                Destroy(v.gameObject);
+            }
+        }
+
+        public void RequestTeleport(Vector3 pos, Quaternion rot)
+        {
+            this.CmdRequestTeleport(pos, rot);
+        }
+
+        [Command]
+        void CmdRequestTeleport(Vector3 pos, Quaternion rot)
+        {
+            if (m_player.OwnedPed != null)
+            {
+                m_player.OwnedPed.Teleport(pos, rot);
+            }
         }
 
     }
