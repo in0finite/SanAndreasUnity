2a163e909041d0b88a01887d2054ae90429f677c
in0finite <leshow816@gmail.com>
2019-07-06 00:52:22 +0200

    add it to namespace

--- Assets/Scripts/Behaviours/CharacterModelChanger.cs
+++ Assets/Scripts/Behaviours/CharacterModelChanger.cs
@@ -1,50 +1,55 @@
 ﻿using SanAndreasUnity.Behaviours;
 using UnityEngine;
 
-public class CharacterModelChanger : MonoBehaviour
+namespace SanAndreasUnity.Behaviours
 {
-    public KeyCode actionKey = KeyCode.P;
 
-    // Use this for initialization
-    private void Start()
+    public class CharacterModelChanger : MonoBehaviour
     {
-    }
+        public KeyCode actionKey = KeyCode.P;
 
-    // Update is called once per frame
-    private void Update()
-    {
-        if (Input.GetKeyDown(actionKey))
+        // Use this for initialization
+        private void Start()
         {
-            if (Utilities.NetUtils.IsServer)
-                ChangePedestrianModel();
-            else if (Net.PlayerRequests.Local != null)
-                Net.PlayerRequests.Local.RequestPedModelChange();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetKeyDown(actionKey))
+            {
+                if (Utilities.NetUtils.IsServer)
+                    ChangePedestrianModel();
+                else if (Net.PlayerRequests.Local != null)
+                    Net.PlayerRequests.Local.RequestPedModelChange();
+            }
+        }
+
+        public static void ChangePedestrianModel()
+        {
+            if (Ped.Instance != null)
+            {
+    			ChangePedestrianModel(Ped.Instance.PlayerModel, -1);
+            }
+        }
+
+        public static void ChangePedestrianModel(PedModel ped, int newModelId)
+        {
+            
+    		if (-1 == newModelId)
+    			newModelId = Ped.RandomPedId;
+    		
+            // Retry with another random model if this one doesn't work
+            try
+            {
+                ped.Load(newModelId);
+            }
+            catch (System.NullReferenceException ex)
+            {
+    			Debug.LogException (ex);
+                ChangePedestrianModel(ped, -1);
+            }
         }
     }
 
-    public static void ChangePedestrianModel()
-    {
-        if (Ped.Instance != null)
-        {
-			ChangePedestrianModel(Ped.Instance.PlayerModel, -1);
-        }
-    }
-
-    public static void ChangePedestrianModel(PedModel ped, int newModelId)
-    {
-        
-		if (-1 == newModelId)
-			newModelId = Ped.RandomPedId;
-		
-        // Retry with another random model if this one doesn't work
-        try
-        {
-            ped.Load(newModelId);
-        }
-        catch (System.NullReferenceException ex)
-        {
-			Debug.LogException (ex);
-            ChangePedestrianModel(ped, -1);
-        }
-    }
 }
