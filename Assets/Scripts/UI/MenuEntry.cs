f400009bcf3dcb26ee9b8aed3ad619cd1cb2b8c3
in0finite <leshow816@gmail.com>
2019-07-09 16:52:57 +0200

    implement MenuEntry

---
+++ Assets/Scripts/UI/MenuEntry.cs
@@ -0,0 +1,25 @@
+using System.Collections.Generic;
+
+namespace SanAndreasUnity.UI
+{
+
+    public class MenuEntry
+    {
+        public string name = "";
+        public int sortPriority = 0;
+        public List<MenuEntry> children = new List<MenuEntry>();
+
+        public void AddChild(MenuEntry entry)
+        {
+            int index = this.children.FindIndex(e => e.sortPriority > entry.sortPriority);
+
+            if (index < 0)
+                this.children.Add(entry);
+            else
+                this.children.Insert(index, entry);
+
+        }
+
+    }
+
+}
