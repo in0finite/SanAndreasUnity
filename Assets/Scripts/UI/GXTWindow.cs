e236f7d032ecd3d6315e8be762e5c46f3c8f86b0
AlexWei <lexnewgate@gmail.com>
2019-08-30 03:17:35 +0800

    Import gxt (#23)
    * import GTX code
    
    import GTX code
    add some empty function before integrate
    
    * set gtx path
    
    * refactor code
    
    * fix typo and ui prefab
    
    change gtx to gxt
    
    remove window from start scene and add it in prefab
    
    * add language interface
    
    add language interface;
    add some todo comments;
    change file tab; make it consistent with project.
    
    * fix game path and typo
    
    add test ui
    
    * add ui part
    
    * fix set gui style pos
    
    it will cause bug in ctor
    
    * add pages to gxt ui
    
    * revert changes to startup scene

---
+++ Assets/Scripts/UI/GXTWindow.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using SanAndreasUnity.Importing.GXT;
+using SanAndreasUnity.Utilities;
+using UnityEngine;
+
+namespace SanAndreasUnity.UI
+{
+	class GXTWindow : PauseMenuWindow
+	{
+		private Vector2 _tableNameScrollPos;
+		private Vector2 _crcScrollPos;
+		private const float WindowWidth = 600;
+		private const float WindowHeight = 400;
+		private Int32? curCrc = null;
+
+		private List<Int32> crcList = new List<int>();
+		private GUIStyle _gxtTextAreaStyle;
+
+		GXTWindow()
+		{
+			//this will use as menu item name as well as window title
+			this.windowName = "GXTWindow";
+
+		}
+
+		void SetUpGxtTextAreaStyle()
+		{
+			_gxtTextAreaStyle = new GUIStyle();
+			_gxtTextAreaStyle.wordWrap = true;
+			_gxtTextAreaStyle.normal.textColor = Color.white;
+			_gxtTextAreaStyle.padding = new RectOffset(20, 20, 10, 5);
+		}
+
+		void Start()
+		{
+			this.RegisterButtonInPauseMenu();
+			this.windowRect = Utilities.GUIUtils.GetCenteredRect(new Vector2(WindowWidth, WindowHeight));
+			SetUpGxtTextAreaStyle();
+
+		}
+
+
+		private int m_currentPageNumber = 1;
+		private int numCrcPerPage = 40;
+		protected override void OnWindowGUI()
+		{
+			base.OnWindowGUI();
+			GUILayout.BeginHorizontal();
+
+			#region TableName
+			GUILayout.BeginVertical(GUILayout.Width(WindowWidth / 3));
+			GUILayout.Label("TableName");
+			_tableNameScrollPos = GUILayout.BeginScrollView(_tableNameScrollPos);
+			foreach (var gxtSubTableName in GXT.Gxt.SubTableNames)
+			{
+				if (GUILayout.Button(gxtSubTableName))
+				{
+					crcList = GXT.Gxt.TableEntryNameDict[gxtSubTableName];
+				}
+			}
+			GUILayout.EndScrollView();
+			GUILayout.EndVertical();
+			#endregion
+
+			#region CRC
+			GUILayout.BeginVertical(GUILayout.Width(WindowWidth / 3));
+			GUILayout.Label("CRC");
+			if (crcList.Count > numCrcPerPage) //only show pages when we really need
+			{
+				m_currentPageNumber = GUIUtils.DrawPagedViewNumbers(GUILayoutUtility.GetRect(WindowWidth/3,20),
+				m_currentPageNumber, crcList.Count, numCrcPerPage);
+			}
+
+			_crcScrollPos = GUILayout.BeginScrollView(_crcScrollPos);
+
+			foreach (var crc in crcList.Skip((m_currentPageNumber-1)*numCrcPerPage).Take(numCrcPerPage))
+			{
+				if (GUILayout.Button(crc.ToString()))
+				{
+					curCrc = crc;
+				}
+			}
+			GUILayout.EndScrollView();
+			GUILayout.EndVertical();
+
+			#endregion
+			GUILayout.BeginVertical(GUILayout.Width(WindowWidth / 3));
+			GUILayout.Label("text");
+			if (curCrc.HasValue)
+			{
+
+				GUILayout.TextArea(GXT.Gxt.EntryNameWordDict[curCrc.Value], _gxtTextAreaStyle);
+			}
+			GUILayout.EndHorizontal();
+			GUILayout.EndVertical();
+
+		}
+	}
+}
