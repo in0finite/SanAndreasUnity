c3382928c26551444d0227b59963666b2ffbfd2f
in0finite <leshow816@gmail.com>
2019-11-12 14:43:22 +0100

    implement network discovery

--- Assets/Scripts/UI/JoinGameWindow.cs
+++ Assets/Scripts/UI/JoinGameWindow.cs
@@ -2,6 +2,7 @@
 using UnityEngine;
 using SanAndreasUnity.Utilities;
 using SanAndreasUnity.Net;
+using System.Linq;
 
 namespace SanAndreasUnity.UI
 {
@@ -10,6 +11,12 @@ namespace SanAndreasUnity.UI
     {
 		[SerializeField] string m_ipStr = "127.0.0.1";
 		string m_portStr = NetManager.defaultListenPortNumber.ToString();
+
+		string[] m_tabNames = new string[]{"Direct", "LAN"};
+		int m_currentTabIndex = 0;
+
+		Mirror.NetworkDiscoveryHUD m_netDiscoveryHUD;
+
 
 
 		JoinGameWindow()
@@ -25,7 +32,13 @@ namespace SanAndreasUnity.UI
 		void Start ()
         {
 			// adjust rect
-			this.windowRect = GUIUtils.GetCenteredRect(new Vector2(550, 300));
+			float width = Mathf.Min(650, Screen.width * 0.9f);
+			this.windowRect = GUIUtils.GetCenteredRect(new Vector2(width, 400));
+
+			m_netDiscoveryHUD = Mirror.NetworkManager.singleton.GetComponentOrThrow<Mirror.NetworkDiscoveryHUD>();
+			m_netDiscoveryHUD.connectAction = this.ConnectFromDiscovery;
+			m_netDiscoveryHUD.drawGUI = false;
+
 		}
 
 		void Update()
@@ -38,18 +51,28 @@ namespace SanAndreasUnity.UI
 		protected override void OnWindowGUI ()
 		{
 			
-			GUILayout.Label ("IP:");
-			m_ipStr = GUILayout.TextField(m_ipStr, GUILayout.Width(200));
+			m_currentTabIndex = GUIUtils.TabsControl(m_currentTabIndex, m_tabNames);
 
-            GUILayout.Label ("Port:");
-			m_portStr = GUILayout.TextField(m_portStr, GUILayout.Width(100));
+			GUILayout.Space(20);
+
+			if (0 == m_currentTabIndex)
+			{
+				GUILayout.Label ("IP:");
+				m_ipStr = GUILayout.TextField(m_ipStr, GUILayout.Width(200));
+
+				GUILayout.Label ("Port:");
+				m_portStr = GUILayout.TextField(m_portStr, GUILayout.Width(100));
+			}
+			else if (1 == m_currentTabIndex)
+			{
+				m_netDiscoveryHUD.width = (int) this.WindowSize.x - 30;
+				m_netDiscoveryHUD.DisplayServers();
+			}
             
 		}
 
 		protected override void OnWindowGUIAfterContent()
 		{
-
-			GUILayout.Space(40);
 
 			// label with status
 			string strStatus = "Disconnected";
@@ -65,10 +88,10 @@ namespace SanAndreasUnity.UI
 			}
 			GUILayout.Label("Status: " + strStatus);
 
-			// button for connecting/disconnecting
+			// button for connecting/disconnecting/refreshing LAN
 
 			string buttonText = "Connect";
-			System.Action buttonAction = this.Connect;
+			System.Action buttonAction = this.ConnectDirectly;
 			if (NetStatus.IsClientConnecting())
 			{
 				buttonText = "Disconnect";
@@ -80,17 +103,36 @@ namespace SanAndreasUnity.UI
 				buttonText = "Connected";
 				buttonAction = () => {};
 			}
+			else
+			{
+				if (1 == m_currentTabIndex)
+				{
+					GUI.enabled = ! m_netDiscoveryHUD.IsRefreshing;
+					buttonText = m_netDiscoveryHUD.IsRefreshing ? ( "Refreshing." + new string('.', (int) ((Time.time * 2) % 3)) ) : "Refresh LAN";
+					buttonAction = () => m_netDiscoveryHUD.Refresh();
+				}
+			}
 
             if (GUIUtils.ButtonWithCalculatedSize(buttonText, 80, 30))
 				buttonAction();
 
 		}
 
-		void Connect()
+		void ConnectDirectly()
+		{
+			this.Connect(m_ipStr, ushort.Parse(m_portStr));
+		}
+
+		void ConnectFromDiscovery(Mirror.NetworkDiscovery.DiscoveryInfo info)
+		{
+			this.Connect(info.EndPoint.Address.ToString(), ushort.Parse( info.KeyValuePairs[Mirror.NetworkDiscovery.kPortKey] ));
+		}
+
+		void Connect(string ip, ushort port)
 		{
 			try
 			{
-				NetManager.StartClient(m_ipStr, ushort.Parse(m_portStr));
+				NetManager.StartClient(ip, port);
 			}
 			catch (System.Exception ex)
 			{
