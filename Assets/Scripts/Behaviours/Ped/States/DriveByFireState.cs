4e86a5f6b1bb41dfee2ead9b0939e9beab07a4d5
in0finite <leshow816@gmail.com>
2020-03-22 19:27:55 +0100

    drive-by firing WIP

---
+++ Assets/Scripts/Behaviours/Ped/States/DriveByFireState.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+namespace SanAndreasUnity.Behaviours.Peds.States
+{
+
+    public class DriveByFireState : DriveByState, IFireState
+    {
+
+        public override void StartFiring()
+        {
+            // ignore
+
+        }
+
+        public virtual void StopFiring()
+        {
+            // switch back to drive-by state
+            m_ped.GetStateOrLogError<DriveByState>().EnterVehicle(this.CurrentVehicle, this.CurrentVehicleSeatAlignment);
+        }
+
+    }
+
+}
