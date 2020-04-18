8f08e1c2cb37ce686c5da88ed74540418f8d2902
in0finite <leshow816@gmail.com>
2020-04-18 23:51:53 +0200

    don't draw menu items from OnGUI()

--- Assets/Scripts/UI/MainMenu.cs
+++ Assets/Scripts/UI/MainMenu.cs
@@ -19,9 +19,6 @@ namespace SanAndreasUnity.UI
 
 		public Color openedWindowTextColor = Color.green;
 
-		private static GUILayoutOption[] s_buttonOptions = new GUILayoutOption[0];
-		public static GUILayoutOption[] ButtonLayoutOptions { get { return s_buttonOptions; } }
-
 		static MenuEntry s_rootMenuEntry = new MenuEntry();
 
 		static bool s_hasMenuEntriesToAdd = false;
@@ -48,37 +45,6 @@ namespace SanAndreasUnity.UI
 
 			// draw main menu gui
 
-			// draw menu entries at bottom of screen
-
-			s_buttonOptions = new GUILayoutOption[]{ GUILayout.MinWidth(minButtonWidth), GUILayout.MinHeight(minButtonHeight) };
-
-			GUILayout.BeginArea (new Rect (0f, Screen.height - (minButtonHeight + spaceAtBottom), Screen.width, minButtonHeight + spaceAtBottom));
-		//	GUILayout.Space (5);
-		//	GUILayout.FlexibleSpace ();
-
-
-			GUILayout.BeginHorizontal ();
-
-			GUILayout.Space (5);
-			GUILayout.FlexibleSpace ();
-
-			// draw registered menu items
-			foreach (var item in s_rootMenuEntry.children)
-			{
-				if (item.drawAction != null)
-					item.drawAction();
-				GUILayout.Space (this.spaceBetweenButtons);
-			}
-
-			GUILayout.FlexibleSpace ();
-			GUILayout.Space (5);
-
-			GUILayout.EndHorizontal ();
-
-			// add some space below buttons
-		//	GUILayout.Space (spaceAtBottom);
-
-			GUILayout.EndArea ();
 
 		}
 
