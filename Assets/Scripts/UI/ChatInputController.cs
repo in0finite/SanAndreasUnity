6786ab5e76dcbb8691db75f3f512687527682db0
in0finite <leshow816@gmail.com>
2020-04-20 22:17:34 +0200

    convert chat input

---
+++ Assets/Scripts/UI/ChatInputController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace SanAndreasUnity.UI
+{
+
+    public class ChatInputController : MonoBehaviour
+    {
+        public InputField inputField;
+        public Button sendButton;
+
+
+        void Start()
+        {
+            this.sendButton.onClick.AddListener(() => SendChatMessage(this.inputField.text));
+        }
+
+        void SendChatMessage(string msg)
+        {
+            this.inputField.text = "";
+
+            if (string.IsNullOrWhiteSpace(msg))
+                return;
+
+            Chat.ChatManager.SendChatMessageToAllPlayersAsLocalPlayer(msg);
+        }
+
+    }
+
+}
