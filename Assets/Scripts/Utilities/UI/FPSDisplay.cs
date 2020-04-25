6100b013df69e0985c3887c54448fb48b08dafd9
in0finite <leshow816@gmail.com>
2020-04-25 20:35:36 +0200

    rename variables

--- Assets/Scripts/Utilities/UI/FPSDisplay.cs
+++ Assets/Scripts/Utilities/UI/FPSDisplay.cs
@@ -7,18 +7,18 @@ namespace SanAndreasUnity.Utilities {
 	
 	public class FPSDisplay : MonoBehaviour {
 
-		private static int fpsTextureWidth = 75;
-		private static int fpsTextureHeight = 25;
-		private static float fpsMaximum = 60.0f;
+		private static int s_fpsTextureWidth = 75;
+		private static int s_fpsTextureHeight = 25;
+		private static float s_fpsMaximum = 60.0f;
 		/*private static float fpsGreen = 50.0f;
         private static float fpsRed = 23.0f;*/
-		private float fpsDeltaTime = 0.0f;
-		private Texture2D fpsTexture = null;
-		private Color[] colors = null;
-		private float[] fpsHistory = new float[fpsTextureWidth];
-		private int fpsIndex = 0;
+		private float m_fpsDeltaTime = 0.0f;
+		private Texture2D m_fpsTexture = null;
+		private Color[] m_colors = null;
+		private float[] m_fpsHistory = new float[s_fpsTextureWidth];
+		private int m_fpsIndex = 0;
 
-		private static bool _showFPS = true;
+		private static bool s_showFPS = true;
 
 		public RawImage fpsImage;
 		public Text fpsText;
@@ -27,24 +27,24 @@ namespace SanAndreasUnity.Utilities {
 
 		void Awake () {
 
-			fpsTexture = new Texture2D(fpsTextureWidth, fpsTextureHeight, TextureFormat.RGBA32, false, true);
+			m_fpsTexture = new Texture2D(s_fpsTextureWidth, s_fpsTextureHeight, TextureFormat.RGBA32, false, true);
 
-			colors = new Color[fpsTexture.width * fpsTexture.height];
+			m_colors = new Color[m_fpsTexture.width * m_fpsTexture.height];
 
-			this.fpsImage.texture = this.fpsTexture;
+			this.fpsImage.texture = this.m_fpsTexture;
 		}
 		
 		void Update () {
 
 			// FPS counting
-			fpsDeltaTime += (Time.unscaledDeltaTime - fpsDeltaTime) * 0.1f;
+			m_fpsDeltaTime += (Time.unscaledDeltaTime - m_fpsDeltaTime) * 0.1f;
 
 			if (Input.GetKeyDown(KeyCode.F10))
-				_showFPS = !_showFPS;
+				s_showFPS = !s_showFPS;
 
-			if (_showFPS)
+			if (s_showFPS)
 			{
-				UpdateTexture(1.0f / fpsDeltaTime);
+				UpdateTexture(1.0f / m_fpsDeltaTime);
 
 				UpdateText();
 			}
@@ -55,57 +55,57 @@ namespace SanAndreasUnity.Utilities {
 		{
 
 			UnityEngine.Profiling.Profiler.BeginSample("Reset texture pixels");
-			int numPixels = fpsTexture.width * fpsTexture.height;
+			int numPixels = m_fpsTexture.width * m_fpsTexture.height;
 			Color backgroundColor = new Color(0.0f, 0.0f, 0.0f, 0.66f); // Half-transparent background for FPS graph
 			for (int i = 0; i < numPixels; i++)
-				colors[i] = backgroundColor;
+				m_colors[i] = backgroundColor;
 			UnityEngine.Profiling.Profiler.EndSample();
 
 			UnityEngine.Profiling.Profiler.BeginSample("Set pixels");
-			fpsTexture.SetPixels(colors);
+			m_fpsTexture.SetPixels(m_colors);
 			UnityEngine.Profiling.Profiler.EndSample();
 
 			// Append to history storage
-			fpsHistory[fpsIndex] = fps;
+			m_fpsHistory[m_fpsIndex] = fps;
 
-			int f = fpsIndex;
+			int f = m_fpsIndex;
 
-			if (fps > fpsHistory.Average())
-				fpsMaximum = fps;
+			if (fps > m_fpsHistory.Average())
+				s_fpsMaximum = fps;
 
 			// Draw graph into texture
 			UnityEngine.Profiling.Profiler.BeginSample("Set fps history pixels");
-			for (int i = fpsTexture.width - 1; i >= 0; i--)
+			for (int i = m_fpsTexture.width - 1; i >= 0; i--)
 			{
-				float graphVal = (fpsHistory[f] > fpsMaximum) ? fpsMaximum : fpsHistory[f]; //Clamps
-				int height = (int)(graphVal * fpsTexture.height / (fpsMaximum + 0.1f)); //Returns the height of the desired point with a padding of 0.1f units
+				float graphVal = (m_fpsHistory[f] > s_fpsMaximum) ? s_fpsMaximum : m_fpsHistory[f]; //Clamps
+				int height = (int)(graphVal * m_fpsTexture.height / (s_fpsMaximum + 0.1f)); //Returns the height of the desired point with a padding of 0.1f units
 
-				float p = fpsHistory[f] / fpsMaximum,
+				float p = m_fpsHistory[f] / s_fpsMaximum,
 				r = Mathf.Lerp(1, 1 - p, p),
 				g = Mathf.Lerp(p * 2, p, p);
 
-				fpsTexture.SetPixel(i, height, new Color(r, g, 0));
+				m_fpsTexture.SetPixel(i, height, new Color(r, g, 0));
 				f--;
 
 				if (f < 0)
-					f = fpsHistory.Length - 1;
+					f = m_fpsHistory.Length - 1;
 			}
 			UnityEngine.Profiling.Profiler.EndSample();
 
 			// Next entry in rolling history buffer
-			fpsIndex++;
-			if (fpsIndex >= fpsHistory.Length)
-				fpsIndex = 0;
+			m_fpsIndex++;
+			if (m_fpsIndex >= m_fpsHistory.Length)
+				m_fpsIndex = 0;
 
 			UnityEngine.Profiling.Profiler.BeginSample("Apply texture");
-			fpsTexture.Apply(false, false);
+			m_fpsTexture.Apply(false, false);
 			UnityEngine.Profiling.Profiler.EndSample();
 
 		}
 
 		void UpdateText()
 		{
-			float fps = 1.0f / fpsDeltaTime;
+			float fps = 1.0f / m_fpsDeltaTime;
 			string text = string.Format("{0:0.0} fps", fps);
 			if (this.fpsText.text != text)
 				this.fpsText.text = text;
