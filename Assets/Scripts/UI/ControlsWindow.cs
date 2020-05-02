f8ac93380bce564b3ea2d2d7d42ff7c327a09ee8
in0finite <leshow816@gmail.com>
2020-05-02 16:47:36 +0200

    update controls window text

--- Assets/Scripts/UI/ControlsWindow.cs
+++ Assets/Scripts/UI/ControlsWindow.cs
@@ -36,24 +36,41 @@ namespace SanAndreasUnity.UI {
 		public static string GetControlsText() {
 
 			return
+
+			"<b>GENERAL</b>\n\n" +
 			"V - Spawn vehicle\n\n" +
 			"P - Change ped model\n\n" +
+			"Mouse scroll - Zoom in / out camera\n\n" +
+			"Esc - Toggle pause menu\n\n" +
+
+			"\n<b>PED</b>\n\n" +
 			"W/A/S/D - Move ped\n\n" +
+			"Right click - Aim\n\n" +
+			"Left click - Fire\n\n" +
 			"Space - Sprint\n\n" +
 			"Alt - Walk\n\n" +
 			"Left shift - Jump\n\n" +
-			"Mouse scroll - Zoom in / out camera\n\n" +
-			"E/Q - Next/previous weapon\n\n" +
-			"Esc - Toggle pause menu\n\n" +
+			"C - Crouch\n\n" +
+			"Left/right while crouch aiming - Roll\n\n" +
+			"E/Q - Switch weapon\n\n" +
+			"G while aiming - Recruit peds to follow you\n\n" +
 			(Utilities.NetUtils.IsServer ?
 			("T - Fly mode\n\n" +
 			"R - Fly through mode\n\n") : "") +
-			"Enter - Enter/exit vehicles\n\n" +
-			"F10 - Toggle FPS counter\n\n" +
+			"Enter - Enter vehicles\n\n" +
+
+			"\n<b>VEHICLE</b>\n\n" +
+			"W/A/S/D - Move vehicle\n\n" +
+			"Space - Handbrake\n\n" +
+			"Right click - Switch to drive-by mode\n\n" +
+			"Enter - Exit vehicle\n\n" +
+			"E/Q as driver - Switch radio station\n\n" +
+			"E/Q as passenger - Switch weapon\n\n" +
+
+			"\n<b>MINIMAP</b>\n\n" +
 			"B - Zoom out minimap\n\n" +
 			"N - Zoom in minimap\n\n" +
-			"M - Open the entire map\n\n" +
-			"F8 - Show more info in the minimap\n\n";
+			"M - Open the entire map\n\n";
 
 		}
 
