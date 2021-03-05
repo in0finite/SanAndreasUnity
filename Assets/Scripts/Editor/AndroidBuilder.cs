a3a8d5cfdfcab15eabc4e40d4cfa5d6a523e6293
in0finite <leshow816@gmail.com>
2021-03-05 23:19:26 +0100

    add script for building for Android from command line

---
+++ Assets/Scripts/Editor/AndroidBuilder.cs
@@ -0,0 +1,16 @@
+﻿using System.IO;
+using UnityEditor;
+using UnityEngine;
+
+namespace SanAndreasUnity.Editor
+{
+    public class AndroidBuilder
+    {
+        static void Build ()
+        {
+            string buildPath = Path.Combine(Application.dataPath, $"../Build/{nameof(SanAndreasUnity)}.apk");
+
+            BuildPipeline.BuildPlayer(EditorBuildSettings.scenes, buildPath, BuildTarget.Android, BuildOptions.None);
+        }
+    }
+}
