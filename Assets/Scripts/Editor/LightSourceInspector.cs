0a3d2a53dccc14687c3fdc40a78d5ca450ce25a9
in0finite <leshow816@gmail.com>
2021-04-18 00:08:49 +0200

    create inspector for LightSource

---
+++ Assets/Scripts/Editor/LightSourceInspector.cs
@@ -0,0 +1,25 @@
+﻿using SanAndreasUnity.Behaviours.World;
+using UnityEditor;
+using UnityEngine;
+
+namespace SanAndreasUnity.Editor
+{
+    [CustomEditor(typeof(LightSource))]
+    public class LightSourceInspector : UnityEditor.Editor
+    {
+
+        public override void OnInspectorGUI()
+        {
+            base.DrawDefaultInspector ();
+
+            GUILayout.Space (10);
+            GUILayout.Label("Light info:");
+            GUILayout.Space (10);
+
+            var lightSource = (LightSource) this.target;
+
+            EditorUtils.DrawAllFieldsInInspector(lightSource.LightInfo);
+        }
+
+    }
+}
