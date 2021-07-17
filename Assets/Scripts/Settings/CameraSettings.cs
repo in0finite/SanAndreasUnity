d1ab4fbd7b52041853354701dc9431f62865a8e3
in0finite <leshow816@gmail.com>
2021-07-17 22:29:12 +0200

    remove "camera far clip plane" setting

--- Assets/Scripts/Settings/CameraSettings.cs
+++ Assets/Scripts/Settings/CameraSettings.cs
@@ -6,18 +6,9 @@ using SanAndreasUnity.Behaviours.Vehicles;
 namespace SanAndreasUnity.Settings {
 
 	public class CameraSettings : MonoBehaviour {
-		
-		static float s_farClipPlane = 1000;
+
 		static float s_fieldOfView = 60;
 
-		OptionsWindow.FloatInput m_farClipPlaneInput = new OptionsWindow.FloatInput() {
-			description = "Far clip plane",
-			minValue = 100,
-			maxValue = 5000,
-			getValue = () => Camera.main != null ? Camera.main.farClipPlane : s_farClipPlane,
-			setValue = (value) => { s_farClipPlane = value; if (Camera.main != null) Camera.main.farClipPlane = value; },
-			persistType = OptionsWindow.InputPersistType.OnStart,
-		};
 		OptionsWindow.FloatInput m_fieldOfViewInput = new OptionsWindow.FloatInput() {
 			description = "Field of view",
 			minValue = 20,
@@ -47,7 +38,7 @@ namespace SanAndreasUnity.Settings {
 
 		void Awake ()
 		{
-			OptionsWindow.RegisterInputs ("CAMERA", m_farClipPlaneInput, m_fieldOfViewInput, m_cameraDistanceFromPed, m_cameraDistanceFromVehicle);
+			OptionsWindow.RegisterInputs ("CAMERA", m_fieldOfViewInput, m_cameraDistanceFromPed, m_cameraDistanceFromVehicle);
 			UnityEngine.SceneManagement.SceneManager.activeSceneChanged += (s1, s2) => OnActiveSceneChanged();
 		}
 
@@ -65,7 +56,6 @@ namespace SanAndreasUnity.Settings {
 			Camera cam = Utilities.F.FindMainCameraEvenIfDisabled();
 			if (cam != null)
 			{
-				cam.farClipPlane = s_farClipPlane;
 				cam.fieldOfView = s_fieldOfView;
 			}
 			
