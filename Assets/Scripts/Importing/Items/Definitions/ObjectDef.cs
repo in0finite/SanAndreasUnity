0ade2e2c2bb3c9f0bc043739bcdd9f3b1f93820b
in0finite <leshow816@gmail.com>
2021-07-17 23:49:16 +0200

    document flags

--- Assets/Scripts/Importing/Items/Definitions/ObjectDef.cs
+++ Assets/Scripts/Importing/Items/Definitions/ObjectDef.cs
@@ -6,23 +6,26 @@ namespace SanAndreasUnity.Importing.Items.Definitions
     public enum ObjectFlag : uint
     {
         None = 0,
-        WetEffect = 1,
-        RenderAtNight = 2,
-        Alpha1 = 4,
-        Alpha2 = 8,
-        RenderAtDay = 16,
-        Interior = 32,
+        WetEffect = 1, // IS_ROAD
+        RenderAtNight = 2, // ?
+        Alpha1 = 4, // DRAW_LAST
+        Alpha2 = 8, // ADDITIVE
+        RenderAtDay = 16, // ?
+        Interior = 32, // ? doors
         NoZBufferWrite = 64,
-        NoCull = 128,
-        DisableDrawDist = 256,
-        Breakable = 512,
-        BreakableCrack = 1024,
+        NoCull = 128, // DONT_RECEIVE_SHADOWS
+        DisableDrawDist = 256, // ?
+        Breakable = 512, // IS_GLASS_TYPE_1
+        BreakableCrack = 1024, // IS_GLASS_TYPE_2
         GarageDoor = 2048,
-        MultiClumpCollide = 4096,
-        WeatherBrightness = 32768,
+        MultiClumpCollide = 4096, // IS_DAMAGABLE
+        IsTree = 8192,
+        IsPalm = 16384,
+        WeatherBrightness = 32768, // DOES_NOT_COLLIDE_WITH_FLYER
         ExplodeHit = 65536,
-        MultiClumpSpray = 1048576,
-        NoBackCull = 2097152
+        MultiClumpSpray = 1048576, // IS_TAG
+        NoBackCull = 2097152,
+        IsBreakableStatue = 4194304,
     }
 
     public interface ISimpleObjectDefinition : IObjectDefinition
