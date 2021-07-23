a6912324474b7be418b8fed723f2c5ee3e4833d7
in0finite <leshow816@gmail.com>
2021-07-23 22:59:44 +0200

    add player name

--- Assets/Scripts/Networking/Player.cs
+++ Assets/Scripts/Networking/Player.cs
@@ -1,9 +1,11 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using UnityEngine;
 using Mirror;
 using SanAndreasUnity.Behaviours;
 using SanAndreasUnity.Utilities;
 using System.Linq;
+using System.Text;
 
 namespace SanAndreasUnity.Net
 {
@@ -27,6 +29,18 @@ namespace SanAndreasUnity.Net
         //public GameObject OwnedGameObject { get { return m_ownedGameObject; } internal set { m_ownedGameObject = value; } }
         public Ped OwnedPed { get { return m_ownedPed; } internal set { m_ownedPed = value; m_ownedGameObject = value != null ? value.gameObject : null; } }
 
+        [SyncVar]
+        private string m_net_playerName = string.Empty;
+        public string PlayerName
+        {
+            get => m_net_playerName;
+            private set => m_net_playerName = value;
+        }
+
+        public static readonly int MaxPlayerNameLength = 20;
+        public static readonly int MinPlayerNameLength = 2;
+        public static readonly string DefaultPlayerName = "Player";
+
         public string DescriptionForLogging => "(netId=" + this.netId + ", addr=" + (this.connectionToClient != null ? this.connectionToClient.address : "") + ")";
 
         private readonly SyncedBag.StringSyncDictionary m_syncDictionary = new SyncedBag.StringSyncDictionary();
@@ -43,6 +57,15 @@ namespace SanAndreasUnity.Net
             if (null == ped)
                 return null;
             return AllPlayersEnumerable.FirstOrDefault(p => p.OwnedPed == ped);
+        }
+
+        private void Awake()
+        {
+            if (NetStatus.IsServer)
+            {
+                // assign default player name
+                this.PlayerName = GeneratePlayerName(DefaultPlayerName);
+            }
         }
 
         void OnEnable()
@@ -136,6 +159,67 @@ namespace SanAndreasUnity.Net
         {
             var go = NetManager.GetNetworkObjectById(netId);
             return go != null ? go.GetComponent<Player>() : null;
+        }
+
+        public static Player GetByName(string name)
+        {
+            return s_allPlayers.Find(p => p.PlayerName == name);
+        }
+
+        public static string ValidatePlayerName(string playerName)
+        {
+            if (null == playerName)
+                return DefaultPlayerName;
+
+            if (playerName.Length < MinPlayerNameLength)
+                return DefaultPlayerName;
+
+            if (playerName.Length > MaxPlayerNameLength)
+                playerName = playerName.Substring(0, MaxPlayerNameLength);
+
+            var sb = new StringBuilder(playerName);
+
+            // remove tags
+            sb.Replace("<", "< "); // the easiest way
+
+            sb.Replace('\r', ' ');
+            sb.Replace('\n', ' ');
+            sb.Replace('\t', ' ');
+
+            // trim has to be done after other operations, because they can produce whitespaces
+            playerName = sb.ToString().Trim();
+
+            if (playerName.Length < MinPlayerNameLength)
+                return DefaultPlayerName;
+
+            return playerName;
+        }
+
+        public static string GeneratePlayerName(string playerName)
+        {
+            playerName = ValidatePlayerName(playerName);
+
+            // check if player with this name already exists
+
+            if (GetByName(playerName) == null)
+                return playerName;
+
+            for (int i = 1; i < 10000; i++)
+            {
+                string newName = playerName + " (" + i + ")";
+                if (null == GetByName(newName))
+                    return newName;
+            }
+
+            throw new Exception("Failed to generate player name");
+        }
+
+        public void RequestNameChange(string newName) => this.CmdRequestNameChange(newName);
+
+        [Command]
+        private void CmdRequestNameChange(string newName)
+        {
+            this.PlayerName = GeneratePlayerName(newName);
         }
 
     }
