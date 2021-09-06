3081db59ad53c83226a6265588298fa3420b9177
in0finite <leshow816@gmail.com>
2021-09-07 00:04:15 +0200

    add StartupSingleton

---
+++ Assets/Scripts/Utilities/StartupSingleton.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using UnityEngine;
+
+namespace SanAndreasUnity.Utilities
+{
+    public class StartupSingleton<T> : MonoBehaviour
+        where T : StartupSingleton<T>
+    {
+        public static T Singleton { get; private set; }
+
+        private void Awake()
+        {
+            if (Singleton != null)
+            {
+                throw new Exception($"Awake() method called twice for singleton of type {this.GetType().Name}");
+            }
+
+            Singleton = (T) this;
+
+            this.OnSingletonAwake();
+        }
+
+        protected virtual void OnSingletonAwake()
+        {
+        }
+
+        private void Start()
+        {
+            if (this != Singleton)
+                return;
+
+            this.OnSingletonStart();
+        }
+
+        protected virtual void OnSingletonStart()
+        {
+        }
+    }
+}
