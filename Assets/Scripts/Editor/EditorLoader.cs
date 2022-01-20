9e890e9f255bfbe2400611487b2d85fdbeaa4d41
in0finite <leshow816@gmail.com>
2022-01-21 00:19:09 +0100

    add EditorLoader which can start loading game data and display loading progress

---
+++ Assets/Scripts/Editor/EditorLoader.cs
@@ -0,0 +1,81 @@
+﻿using SanAndreasUnity.Behaviours;
+using SanAndreasUnity.Utilities;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+namespace SanAndreasUnity.Editor
+{
+    public class EditorLoader
+    {
+        [InitializeOnLoadMethod]
+        static void Init()
+        {
+            EditorApplication.update -= EditorUpdate;
+            EditorApplication.update += EditorUpdate;
+        }
+
+        static void EditorUpdate()
+        {
+            if (EditorApplication.isPlaying)
+                return;
+
+            if (Loader.IsLoading)
+            {
+                if (EditorUtility.DisplayCancelableProgressBar("Loading game data", Loader.LoadingStatus, Loader.GetProgressPerc()))
+                {
+                    Loader.StopLoading();
+                    EditorUtility.ClearProgressBar();
+                    return;
+                }
+            }
+        }
+
+        [MenuItem(EditorCore.MenuName + "/" + "Load game data")]
+        static void MenuItemLoadGameData()
+        {
+            if (EditorApplication.isPlaying)
+            {
+                EditorUtility.DisplayDialog("", "Exit play mode first.", "Ok");
+                return;
+            }
+
+            if (null == Loader.Instance)
+            {
+                new GameObject("Loader", typeof(Loader));
+            }
+
+            Loader.StartLoading();
+        }
+
+        [MenuItem(EditorCore.MenuName + "/" + "Change path to GTA")]
+        static void MenuItemChangePath()
+        {
+            if (EditorApplication.isPlaying)
+            {
+                EditorUtility.DisplayDialog("", "Exit play mode first.", "Ok");
+                return;
+            }
+
+            Config.Load();
+
+            string selectedFolder = EditorUtility.OpenFolderPanel("Select GTA installation folder", Config.GamePath ?? "", "");
+            if (string.IsNullOrWhiteSpace(selectedFolder))
+            {
+                return;
+            }
+
+            if (!Loader.IsGamePathCorrect(selectedFolder, out string errorMessage))
+            {
+                EditorUtility.DisplayDialog("", "Selected folder is not valid:\r\n\r\n" + errorMessage, "Ok");
+                return;
+            }
+
+            Config.SetString(Config.const_game_dir, selectedFolder);
+            Config.SaveUserConfig();
+
+            EditorUtility.DisplayDialog("", "Successfully changed path.", "Ok");
+        }
+    }
+}
