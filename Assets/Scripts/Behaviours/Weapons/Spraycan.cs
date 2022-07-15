51e80f89f285cfebdf9380b01dcb1fa43bf4e34f
Quickz <alexserg@inbox.lv>
2022-07-15 19:08:31 +0300

    corrected the idle, walk, run, aim/fire animation for the spraycan

---
+++ Assets/Scripts/Behaviours/Weapons/Spraycan.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using SanAndreasUnity.Importing.Animation;
+
+namespace SanAndreasUnity.Behaviours.Weapons
+{
+	public class Spraycan : Weapon
+	{
+		public override AnimId AimAnim => new AnimId("SPRAYCAN", "spraycan_fire");
+		public override AnimId IdleAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Idle);
+		public override AnimId WalkAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Walk);
+		public override AnimId RunAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Run);
+	}
+}
