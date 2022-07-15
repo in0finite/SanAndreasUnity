31c029284fe9561844ece282442b94f6bfaf4e98
Quickz <alexserg@inbox.lv>
2022-07-15 17:49:51 +0300

    corrected animations for the MP5

--- Assets/Scripts/Behaviours/Weapons/MP5.cs
+++ Assets/Scripts/Behaviours/Weapons/MP5.cs
@@ -3,16 +3,11 @@ using SanAndreasUnity.Importing.Animation;
 
 namespace SanAndreasUnity.Behaviours.Weapons
 {
-
 	public class MP5 : Weapon
 	{
-		
-		public override AnimId AimAnim {
-			get {
-				return new AnimId (AnimGroup.Uzi, AnimIndex.UZI_fire);
-			}
-		}
-
+		public override AnimId AimAnim => new AnimId (AnimGroup.Uzi, AnimIndex.UZI_fire);
+		public override AnimId IdleAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Idle);
+		public override AnimId WalkAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Walk);
+		public override AnimId RunAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Run);
 	}
-
 }
