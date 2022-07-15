670b60cde70dcea49cd9986dc52f1f5a3be87141
Quickz <alexserg@inbox.lv>
2022-07-15 17:46:44 +0300

    corrected the aim, idle, walk, run animation for the extinguisher - added a weapon class for it

---
+++ Assets/Scripts/Behaviours/Weapons/Extinguisher.cs
@@ -0,0 +1,12 @@
+﻿using SanAndreasUnity.Importing.Animation;
+
+namespace SanAndreasUnity.Behaviours.Weapons
+{
+	public class Extinguisher : Weapon
+	{
+		public override AnimId AimAnim => new AnimId(AnimGroup.Flame, AnimIndex.FLAME_fire);
+		public override AnimId IdleAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Idle);
+		public override AnimId WalkAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Walk);
+		public override AnimId RunAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Run);
+	}
+}
