5b1c36ec2c86fe36256e4e61ba259f7af89ae01c
Quickz <alexserg@inbox.lv>
2022-07-15 17:45:35 +0300

    added the correct aim animation for the minigun and the flame thrower

--- Assets/Scripts/Behaviours/Weapons/FThrower.cs
+++ Assets/Scripts/Behaviours/Weapons/FThrower.cs
@@ -3,16 +3,8 @@ using SanAndreasUnity.Importing.Animation;
 
 namespace SanAndreasUnity.Behaviours.Weapons
 {
-
 	public class FThrower : Weapon
 	{
-		
-//		public override AnimId AimAnim {
-//			get {
-//				return new AnimId (AnimGroup.Flame, AnimIndex.FLAME_fire);
-//			}
-//		}
-
+		public override AnimId AimAnim => new AnimId(AnimGroup.Flame, AnimIndex.FLAME_fire);
 	}
-
 }
