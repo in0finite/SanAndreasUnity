dbe0220de2b33f60eef0bcbd2c57afb58dc9c671
Aleksejs Sergejevs <17292064+Quickz@users.noreply.github.com>
2022-07-17 10:08:40 +0300

    Weapon animation changes (#134)
    * added the correct aim animation for the minigun and the flame thrower
    
    * corrected the aim, idle, walk, run animation for the extinguisher - added a weapon class for it
    
    * corrected animations for the MP5
    
    * corrected the idle, walk, run, aim/fire animation for the spraycan

--- Assets/Scripts/Behaviours/Weapons/Extinguisher.cs
+++ Assets/Scripts/Behaviours/Weapons/Extinguisher.cs
@@ -1,11 +1,10 @@
-﻿using UnityEngine;
-using SanAndreasUnity.Importing.Animation;
+﻿using SanAndreasUnity.Importing.Animation;
 
 namespace SanAndreasUnity.Behaviours.Weapons
 {
-	public class MP5 : Weapon
+	public class Extinguisher : Weapon
 	{
-		public override AnimId AimAnim => new AnimId (AnimGroup.Uzi, AnimIndex.UZI_fire);
+		public override AnimId AimAnim => new AnimId(AnimGroup.Flame, AnimIndex.FLAME_fire);
 		public override AnimId IdleAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Idle);
 		public override AnimId WalkAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Walk);
 		public override AnimId RunAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Run);
--- Assets/Scripts/Behaviours/Weapons/Spraycan.cs
+++ Assets/Scripts/Behaviours/Weapons/Spraycan.cs
@@ -3,9 +3,9 @@ using SanAndreasUnity.Importing.Animation;
 
 namespace SanAndreasUnity.Behaviours.Weapons
 {
-	public class MP5 : Weapon
+	public class Spraycan : Weapon
 	{
-		public override AnimId AimAnim => new AnimId (AnimGroup.Uzi, AnimIndex.UZI_fire);
+		public override AnimId AimAnim => new AnimId("SPRAYCAN", "spraycan_fire");
 		public override AnimId IdleAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Idle);
 		public override AnimId WalkAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Walk);
 		public override AnimId RunAnim => new AnimId(AnimGroup.WalkCycle, AnimIndex.Run);
